84e2d6d569e6598042c5e0df645d30b26cbde9fc
Cabbage <cabbagepenetrator@gmail.com>
2022-04-26 22:08:50 +0100

    Extend Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          900: '#181820',
+        },
+        fuchsia: {
+          500: '#BB3FDD',
+          700: '#8D30A4',
+        },
+        rose: {
+          400: '#FF9F7C',
+        },
+        zinc: {
+          500: '#9E9EA1',
+          800: '#30303D',
+        },
+      },
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
