0633f759c143f8a8da1c0758f869e56991eb265f
Cabbage <cabbagepenetrator@gmail.com>
2022-04-29 23:51:48 +0100

    Showing page title

--- src/components/layouts/Auth.vue
+++ src/components/layouts/Auth.vue
@@ -1,8 +1,19 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+const $route = useRoute()
+</script>
+
 <template>
-  <div>
-    <div class="flex justify-between items-center">
-      <slot name="" />
+  <div class="px-6">
+    <div class="flex justify-between items-center pt-8">
+      <h1 class="text-xl">{{ $route.meta.title }}</h1>
+      <div class="bg-purple-500 rounded-full w-10 h-10 grid place-items-center">
+        JD
+      </div>
     </div>
-    <slot />
+    <div class="pt-10">
+      <slot />
+    </div>
   </div>
 </template>
