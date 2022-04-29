5d9e9cdcd8fe44f60afd983c569fd6fd3eab307d
Cabbage <cabbagepenetrator@gmail.com>
2022-04-29 23:52:07 +0100

    Added dashboard header

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -3,5 +3,18 @@ import Auth from '@/components/layouts/Auth.vue'
 </script>
 
 <template>
-  <Auth>dashboard</Auth>
+  <Auth>
+    <h2 class="text-4xl">
+      <div>Good morning,</div>
+      <div>Jane Doe</div>
+    </h2>
+    <div class="flex gap-4 pt-14">
+      <button class="bg-zinc-600 px-5 py-2 rounded-2xl">Daily overview</button>
+      <button
+        class="px-5 py-2 rounded-2xl border-2 border-zinc-600 border-opacity-50"
+      >
+        Statistics
+      </button>
+    </div>
+  </Auth>
 </template>
